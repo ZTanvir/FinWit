55bbc0551d0f4ff4904b5af3d5a76178f4ef2286
zahirul <zahirulopel@gmail.com>
2024-10-27 18:47:38 +0600

    income_categories table added

--- database/schema.sql
+++ database/schema.sql
@@ -6,4 +6,30 @@ CREATE TABLE IF NOT EXISTS users(
     mobile_number VARCHAR(50) NOT NULL,
     birthday DATE NOT NULL,
     password VARCHAR(255) NOT NULL
-)
+);
+
+CREATE TABLE IF NOT EXISTS income_categories(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    name VARCHAR(50),
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+-- CREATE TABLE IF NOT EXISTS incomes(
+--     id SERIAL PRIMARY KEY,
+--     user_id SERIAL,
+--     category_id SERIAL,
+--     title VARCHAR(255) NOT NULL,
+--     amount NUMERIC(8,5) NOT NULL,
+--     date DATE NOT NULL DEFAULT CURRENT_DATE,
+--     time TIME NOT NULL DEFAULT CURRENT_TIME,
+--     message TEXT NOT NULL,
+--     PRIMARY KEY (user_id,category_id),
+--     CONSTRAINT fk_users
+--         FOREIGN KEY(user_id)
+--         REFERENCES users(id)
+--         ON DELETE CASCADE
+-- );
