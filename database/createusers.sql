09d11075d739724c98f124be8f5fc09e25f5d01d
zahirul <zahirulopel@gmail.com>
2024-11-12 19:40:01 +0600

    add createusers sql

---
+++ database/createusers.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+    users(
+        fullname,username,email,mobile_number,birthday,password
+    )
+VALUES
+    ('Zahirul Islam Tanvir','ztanvir','abc@gmail.com','01661514578','1996-08-29','tanvir'),
+    ('Altaf raju','altaf','altafraju@gmail.com','0177515845','1996-02-24','altaf');
